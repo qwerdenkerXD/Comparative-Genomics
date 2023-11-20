93be0bacd4722249350bf9049e00463ddc5d4030
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-11-20 10:33:48 +0100

    methods: add script downloading genomes

---
+++ Methods/download_genomes.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+# SEE:
+# https://hpc-uit.readthedocs.io/en/latest/jobs/examples.html#general-blueprint-for-a-jobscript
+
+##############################
+#       Job blueprint        #
+##############################
+
+# Give your job a name, so you can recognize it in the queue overview
+#SBATCH --job-name=download_genome_data
+
+# You can further define the number of tasks with --ntasks-per-*
+# See "man sbatch" for details. e.g. --ntasks=4 will ask for 4 cpus.
+#SBATCH --ntasks=1
+
+# How much memory you need.
+# --mem will define memory per node and
+# --mem-per-cpu will define memory per CPU/core. Choose one of those.
+#SBATCH --mem-per-cpu=1MB
+##SBATCH --mem=5GB    # this one is not in effect, due to the double hash
+
+# Define custom output and error files:
+# See
+# https://unix.stackexchange.com/questions/285690/slurm-custom-standard-output-name
+#SBATCH --output=%x_%j_slurm.out
+#SBATCH --error=%x_%j_slurm.err
+
+# This is where the actual work is done. In this case, the script only waits.
+# The time command is optional, but it may give you a hint on how long the
+# command worked
+
+curl -H "Accept: application/zip" -o     "../Material/S. cerevisiae.zip"       "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000146045.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000146045.2.zip"
+curl -H "Accept: application/zip" -o     "../Material/E. gossypii.zip"         "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000091025.4/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000091025.4.zip"
+curl -H "Accept: application/zip" -o     "../Material/K. lactis.zip"           "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000002515.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000002515.2.zip"
+curl -H "Accept: application/zip" -o     "../Material/N. glabratus.zip"        "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000002545.3/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000002545.3.zip"
+curl -H "Accept: application/zip" -o     "../Material/L. thermotolerans.zip"   "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000142805.1/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000142805.1.zip"
+curl -H "Accept: application/zip" -o     "../Material/L. kluyveri.zip"         "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCA_000149225.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCA_000149225.2.zip"
+
+cd ../Material
+mkdir Genomes
+unzip -o "*.zip" -d Genomes
+rm *.zip
+
+# Finish the script
+exit 0
---
+++ README.md
@@ -0,0 +1,43 @@
+# Dies ist das Laborbuch der Übung im Modul Komparative Genomik
+
+Was tragen wir hier ein?
+
+Jeden Schritt, der Daten einliest und Daten verarbeitet und ein
+(Zwischen)-Ergebnis produziert. Mit allen Parametern, mit der Version des Codes
+(Script), die den Schritt durchgeführt hat, und wo die Ausgabe zu finden ist.
+
+## Material
+
+Für unsere komparative Genomik der Bäckerhefe verwenden wir Genome von nahen
+Verwandten:
+
+- _S. cerevisiae_ - Bäckerhefe
+- _E. gossypii_ - Schimmelpilz. Ein Pathogen bei Baumwolle und Zitrusfrüchten.
+  wächst filamentös.
+  Referenz-Genom: https://www.ncbi.nlm.nih.gov/datasets/taxonomy/284811/
+- _K. lactis_ - Milchsäuregärung bei Kefir
+- _N. clabratus_ - Symbiontischer Pilz des Menschen in den Schleimhäuten. Kann
+  Krankheit bei Immunschwäche verursachen.
+- _L. thermotolerans_ - Milchsäuregärung bei niedrigen und hohen Temperaturen.
+  Kommt in natürlich fermentierten Lebensmitteln vor. Wird genannt im
+  Zusammenhang mit Fruchtfliegen (nachlesen).
+- _L. kluyveri_ - Verwandter der Bäckerhefe, fermentiert nur in klar anaeroben
+  Bedingungen. Kommt vor in "slime flux" eine Erkrankung von Bäumen in Nordamerika
+  und Europa.
+
+### Download der Referenz-Genome
+
+Siehe Script `./Methods/download_genomes.sh`
+
+- _S. cerevisiae_
+- _E. gossypii_
+- _K. lactis_
+- _N. clabratus_
+- _L. thermotolerans_
+- _L. kluyveri_
+
+Ausführung der Scripte am 24.Oktober:
+```sh
+```
+
+## Methoden
