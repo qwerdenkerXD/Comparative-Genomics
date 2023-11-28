08935864ee55bbefe6ae139f3ea722acf6d806c3
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-11-28 07:26:09 +0100

    methods: fix ortho finding with outgroup

--- Methods/find_orthologous_genes_with_outgroup.sh
+++ Methods/find_orthologous_genes_with_outgroup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 
 # name
-#SBATCH --job-name=$(basename ${0%.*})
+#SBATCH --job-name=find_orthologous_genes_with_outgroups
 
 # cpu
 #SBATCH --ntasks=40
 
 #SBATCH --mem-per-cpu=1GB
 
-methods_dir="$(dirname "$0")"
+#SBATCH --output=./logs/%x_%j_slurm.out
+#SBATCH --error=./logs/%x_%j_slurm.err
 
-#SBATCH --output=$methods_dir/logs/%x_%j_slurm.out
-#SBATCH --error=$methods_dir/logs/%x_%j_slurm.err
+methods_dir="$(dirname "$0")"
 
 # takes one argument that is the path to 'orthofinder.py'
 if [ $1 ] && [ -f $1 ]
@@ -24,7 +24,7 @@ then
 
     tmp="$methods_dir/../Results/tmp"
     mkdir "$tmp"
-    cp "$methods_dir/../Results/Prediction/*" "$tmp"
+    cp "$methods_dir"/../Results/Prediction/* "$tmp"
     cp "$methods_dir/../Material/Outgroup/ncbi_dataset/data/GCF_027921745.1/protein.faa" "$tmp/GCF_027921745.1_Schizosaccharomyces osmophilus.faa"
     python3 $1 -p logs -f "$methods_dir/../Results/tmp" -o "$methods_dir/../Results/Orthologous_Genes_With_Outgroup"
     rm -r "$tmp"
