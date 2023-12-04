f0cc602ada73fe3f57078779a2db0bb739704612
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-12-04 21:27:01 +0100

    methods: correcting orthofinder evaluation

--- Methods/evaluate_detected_orthologs.R
+++ Methods/evaluate_detected_orthologs.R
@@ -1,22 +1,24 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 families <- read.table(args[1], sep="\t", header=TRUE)
-families.rows <- families[, -1]
-families.sizes <- apply(families.rows, 1, function(family) sum(family!=""))
+families.species <- families[, -1]  # without the Orthogroup name column: "OG00..."
+families.sizes <- apply(families.species, 1, function(family) sum(length(unlist(strsplit(as.character(family), ", ")))))
 
 png("../Results/gene_family_sizes.png", 1000, 1000)
 families.sizes.dist <- table(families.sizes)
-families.sizes.plot <- barplot(families.sizes.dist, ylim=c(0, max(families.sizes.dist) + 500), xlab="sizes", ylab="frequency", main="gene family sizes")
+families.sizes.plot <- barplot(families.sizes.dist, ylim=c(0, max(families.sizes.dist) + 500), xlab="size", ylab="frequency", main="gene family sizes")
 text(x = families.sizes.plot, y = families.sizes.dist, labels = families.sizes.dist, pos = 3, cex = 0.8, col = "red")
 dev.off()
 
 install.packages("venn")
 library(venn)
 
-# Convert the data to a list of vectors
-data_list <- lapply(families.rows, function(x) unlist(strsplit(as.character(x), ", ")))
+# create an associative list {species_name: [gene_family_index, ...], ...} -> all species are associated to
+# a vector of the indices of the gene families they are related to (starting from 1), or NA if not related
+data_list <- lapply(families.species, function(x) ifelse(x != "", seq_along(x), NA))
 
-# Create the Venn diagram
+# create the venn diagram -> the species group together if they share the index of a gene family
+# -> because they share the NA for non-related families, there is one mismatch in the final diagram
 png("../Results/gene_families_venn.png", 1000, 1000)
 venn(data_list, zcolor = "style", box = FALSE)
 dev.off()
